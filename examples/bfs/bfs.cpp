4d9ee335e77d4b70eca1de345202a683a1489e2b
greg <go2432@wayne.edu>
2023-01-04 02:01:32 -0600

    added a reduction method

--- examples/bfs/bfs.cpp
+++ examples/bfs/bfs.cpp
@@ -230,12 +230,11 @@ int main(int argc, char *argv[]) {
     auto dist_i = dist.get_access<read_write_t>(h);
 
     h.parallel_for_work_group(sycl::range<1>(num_work_items), sycl::range<1>(work_group_size),
-    //h.parallel_for_work_group(sycl::nd_range<1>(num_work_items, work_group_size),
                    [=](sycl::nd_item<1> item) { 
                       int glob_id = item.get_global_id();
                       int loc_id = item.get_local_id();
                       for (unsigned int i = loc_id; i < work_group_size; i += 1)
-                        dist_i[glob_id+i] = degree_i[glob_id+i]; 
+                        dist_i[glob_id+i] += degree_i[glob_id+i]; 
                     });
   };
 
