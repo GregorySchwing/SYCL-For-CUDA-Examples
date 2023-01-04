d27a2fecace2bfb0629ee00f6b33948a5dd4e9f1
greg <go2432@wayne.edu>
2023-01-03 22:25:29 -0600

    why segfault?

--- examples/bfs/bfs.cpp
+++ examples/bfs/bfs.cpp
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
 
   CSRGraph graph = createCSRGraphFromFile(config.graphFileName);
   performChecks(graph, config);
-
+  printf("finished checking\n");
   constexpr const size_t N = 100000;
   const sycl::range RowSize{graph.vertexNum+1};
   const sycl::range ColSize{graph.edgeNum*2};
