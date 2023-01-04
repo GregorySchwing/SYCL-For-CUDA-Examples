8a0d4eef4db68f8c94b1df58201963303333aa84
greg <go2432@wayne.edu>
2023-01-03 22:28:24 -0600

    why segfault?

--- examples/bfs/bfs.cpp
+++ examples/bfs/bfs.cpp
@@ -91,7 +91,7 @@ int main(int argc, char *argv[]) {
     const auto read_t = sycl::access::mode::read;
     auto h_e = bufE.get_access<read_t>();
     double sum = 0.0f;
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < graph.vertexNum; i++) {
       sum += h_e[i];
     }
     std::cout << "Sum is : " << sum << std::endl;
